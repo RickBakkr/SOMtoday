76ea329a4743ac9ea6dccf8c999c4436cdf0aa4b
Miniontoby <tobias.gaarenstroom@gmail.com>
2022-01-25 12:00:37 -0700

    Fixed example

--- example.php
+++ example.php
@@ -6,19 +6,6 @@
  * Time: 20:09
  */
 
-function strToHex($string){
-	$hex = '';
-	for ($i = 0; $i < strlen($string); $i++) {
-		$ord = ord($string[$i]);
-		$hexCode = dechex($ord);
-		$hex .= substr('0' . $hexCode, -2);
-	}
-	return strToUpper($hex);
-}
-function encodePassword($password) {
-	return strtolower(strToHex(base64_encode(sha1($password, true))));
-}
-
 include_once("./src/SOMtodayAPI/SOMtoday.php");
 use SOMtodayAPI\SOMtoday;
 
@@ -37,10 +24,10 @@ foreach($allSchools as $school){
 $som = new SOMtodayAPI\SOMtoday($uuid, $username, $password);
 
 // Array of Student objects
-// $students = $som->getStudents();
+$students = $som->getStudents();
 
 // One specific student
-// $student = $som->getStudent(3061729886130);
+$student = $som->getStudent(3061729886130);
 
 // Get grades belonging to a specific student
-// $student->getGrades();
+$student->getGrades();
